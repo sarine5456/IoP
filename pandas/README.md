cf99389cd78b64fca40f8dba63fac5dd404392d5
diego <diegopajarito@gmail.com>
2019-05-07 15:46:55 +0200

    Adding code for using basic functions with pandas

---
+++ pandas/README.md
@@ -0,0 +1,12 @@
+# IoP: Internet of People
+## This is the compilation of the IAAC 2019 seminar for the MACT
+
+This repository is the compilation of a series of tools used during the seminar to gather data from multiple sources.
+
+* **Data**: This [folder](data) contains a set of data sets used for defining the area(s) of study and other basic decriptors.
+
+* **tweepy**: This [folder](tweepy) contains the scripts for listening to tweets using the [tweepy](https://github.com/tweepy/tweepy) library.
+
+
+## Contact: 
+For any comment or suggestion, feel free to either set issues for the repository or simply drop an email to me at [diego.pajarito@iaac.net](mailto:diego.pajarito@iaac.net)
---
+++ pandas/managing_time.py
@@ -0,0 +1,57 @@
+
+# license: Creative Commons License
+# Title: Data management with pandas www.iaac.net
+# Created by: Diego Pajarito
+#
+# is licensed under a license Creative Commons Attribution 4.0 International License.
+# http://creativecommons.org/licenses/by/4.0/
+# This script uses pandas for data management for more information visit; pandas.pydata.org/
+# This script uses geopandas for data management for more information visit; geopandas.org/
+
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+######################################################
+# Read a csv file
+tweets = pd.read_csv('data/tweets_s.csv')
+
+# Time data formats are diverse. one of those formats (and the most reliable) is milliseconds after the epoch
+
+# Raw data in miliseconds
+print('This is an example of time in milliseconds')
+print(tweets['timestamp_ms'][10])
+
+# Using pandas function to transform ms into readable datetime values (be careful with the units
+print('Now the same example in a human-readable datetime format')
+print(pd.to_datetime(tweets['timestamp_ms'][10], unit='ms'))
+
+# adding a new column for datetime values
+tweets['time'] = pd.to_datetime(tweets['timestamp_ms'], unit='ms')
+
+
+
+######################################################
+# Plots using time
+fig, ax = plt.subplots()
+ax.scatter(tweets['time'], tweets['user_screen_name'], alpha=0.3, s=2,)
+
+plt.title('Scatter plot using tweets')
+plt.show()
+
+
+# Changing y-axis value order and adding colors
+tweets.sort_values('user_screen_name', ascending = False, inplace= True)
+fig, ax = plt.subplots()
+#ax.set_yticklabels()
+ax.scatter(tweets['time'], tweets['user_screen_name'], s=2)
+
+plt.title('Scatter ordered by user posting tweets')
+
+
+# Changing y-axis value order and adding colors
+tweets_per_user = tweets['user_screen_name'].value_counts()
+tweets_per_user.plot(kind='bar')
+
+print('end of graphs')
