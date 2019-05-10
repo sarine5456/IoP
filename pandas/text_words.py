4bcee5856c0a3bb0ac546ef15690adfded4f9126
diego <diegopajarito@gmail.com>
2019-05-10 12:04:29 +0200

    First attempt to count words within a text column

--- pandas/text_words.py
+++ pandas/text_words.py
@@ -6,11 +6,12 @@
 # is licensed under a license Creative Commons Attribution 4.0 International License.
 # http://creativecommons.org/licenses/by/4.0/
 # This script uses pandas for data management for more information visit; pandas.pydata.org/
-# This script uses geopandas for data management for more information visit; geopandas.org/
 
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 from pandas import plotting
+import itertools
 
 
 plotting.register_matplotlib_converters()
@@ -20,17 +21,22 @@ plotting.register_matplotlib_converters()
 # Read a csv file
 tweets = pd.read_csv('../data/tweets_s.csv')
 
+# adding a new column for datetime values
+tweets['time'] = pd.to_datetime(tweets['timestamp_ms'], unit='ms')
 
-# Counting tweets per user and showing the count in a bar chart
-tweets_per_user = tweets['user_screen_name'].value_counts()
-# fig, ax = plt.subplots()
-# tweets_per_user.plot(kind='barh', title="Tweets per user")
-# plt.show()
 
-# top users
-top_users = tweets_per_user.nlargest(20)
-fig, ax = plt.subplots()
-top_users.plot(kind='barh', title="Top users")
-plt.show()
+# Counting individual words from the text
+text_example = tweets['text'][20]
+print('A tweet text with the number of words:')
+print(text_example)
+words_example = re.split(r"\s", text_example)
+print('Words: %i' % len(words_example))
 
-print('end of graphs')
+
+# Counting words from the entire set of tweets
+words = tweets['text'].str.split(r"\s")
+words = pd.DataFrame(itertools.chain(*words))
+
+print('Total words: %i' % len(words))
+
+
